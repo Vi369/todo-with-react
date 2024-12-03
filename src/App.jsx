590ca9e75d46db6a66f54ddca18522830c8d66f6
Vikas singh varma <Vikashverma3001@gmail.com>
2024-12-03 17:22:09 +0530

    add all stateHandlers func

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import {addTodo, updateTodo, removeTodo, toggleIsCompleted} from "./TodoFunctions/functions.js"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [todos, setTodos] = useState([]);
+
+    // handle state changes for add todo
+    const handleAddTodo = (newTodo)=>{
+        setTodos((prevTodos)=> addTodo(prevTodos, newTodo))
+    }
+
+    // handle state changes for update todo
+    const handleUpdateTodo = (id, updatedTodo)=>{
+        setTodos((prevTods)=> updateTodo(prevTods, id, updatedTodo));
+    }
+
+    // handle state changes for remove todo
+    const handleRemoveTodo = (id)=>{
+        setTodos((prevTodo)=> removeTodo(prevTodo, id));
+    }
+
+    // handle state changes for toggle completed
+    const handleToggleIsCompleted = (id)=>{
+        setTodos((prevTodo)=> toggleIsCompleted(prevTodo, id));
+    }
 
   return (
-    <>
-      <h1 className='text-3xl'>Hello</h1>
-    </>
+    <TodoProvider value={{todos,
+        addTodo : handleAddTodo,
+        updateTodo : handleUpdateTodo,
+        removeTodo : handleRemoveTodo,
+        toggleIsCompleted : handleToggleIsCompleted
+    }}>
+        
+    </TodoProvider>
   )
 }
 
