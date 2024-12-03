a4af82c41f569f007ab68a237faad554a839c705
Vikas singh varma <Vikashverma3001@gmail.com>
2024-12-03 17:20:07 +0530

    add All functionalities for todo

---
+++ src/TodoFunctions/functions.js
@@ -0,0 +1,23 @@
+const addTodo = (todos, newTodo) => {
+    return [{...newTodo, id: Date.now()}, ...todos];
+}
+
+const updateTodo = (todos, id, updatedTodo)=>{
+    return todos.map((todo)=> {
+        return todo.id === id ? {...todo, ...updatedTodo} : todo
+    })
+}
+
+
+const removeTodo = (todos, id)=> {
+    return todos.filter((todo)=> (todo.id !== id));
+}
+
+const toggleIsCompleted = (todos, id)=>{
+    if(todos && todos.length > 1){
+        todos.map((todo)=>(todo.id ===id ? {...todo, isCompleted : !todo.isCompleted} : todo))
+    }
+}
+
+
+export { addTodo, updateTodo, removeTodo, toggleIsCompleted}
