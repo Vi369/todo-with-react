769624c9db822855bf420b8f28292487e31f86ee
Vikas singh varma <Vikashverma3001@gmail.com>
2024-12-04 18:28:18 +0530

    error fix

--- src/TodoFunctions/functions.js
+++ src/TodoFunctions/functions.js
@@ -14,8 +14,11 @@ const removeTodo = (todos, id)=> {
 }
 
 const toggleIsCompleted = (todos, id)=>{
-    if(todos && todos.length > 1){
-        todos.map((todo)=>(todo.id ===id ? {...todo, isCompleted : !todo.isCompleted} : todo))
+    if(todos && todos.length >= 1){
+        console.log("toggle call in function part")
+        return todos?.map((todo)=>(todo.id ===id ? {...todo, 
+            isCompleted : !todo.isCompleted} : todo))
+            console.log(todos)
     }
 }
 
